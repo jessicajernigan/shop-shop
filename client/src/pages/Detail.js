773cfbe03610b2acf7039e941cef67ebb12407f0
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-10-12 18:33:28 -0500

    added cart toggle and removed hardcoded values -- end of 22.2.6

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
+import Cart from '../components/Cart';
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+
+
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-  
   const [currentProduct, setCurrentProduct] = useState({})
-  
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
   const { products } = state;
-  
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
+
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
@@ -62,6 +73,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
