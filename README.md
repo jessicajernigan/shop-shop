ae3f83de75835299484d6dcad6cb338daf4d9b65
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-10-14 20:12:05 -0500

    22.4.6 - error introduced to login flow at some point. need to revisit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # shop-shop
+
+curl https://api.stripe.com/v1/prices \
+  -u sk_test_4eC39HqLyjWDarjtT1zdp7dc: \
+  -d product="price_1HcKip2eZvKYlo2CHzZglJOO" \
+  -d unit_amount=2000 \
+  -d currency=usd
+
+
+  price_1HcKip2eZvKYlo2CHzZglJOO
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,6 +5,7 @@ import { useStoreContext } from "../utils/GlobalState";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import Cart from '../components/Cart';
+import { idbPromise } from "../utils/helpers";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -26,10 +27,14 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
+
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
 
     if (itemInCart) {
       dispatch({
@@ -37,25 +42,48 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
-  };
+  }
 
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } else if (data) {
+    }
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
@@ -75,9 +103,11 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
-              Add to Cart
+            <button
+              onClick={addToCart}>
+              Add to cart
             </button>
+
             <button
               disabled={!cart.find(p => p._id === currentProduct._id)}
               onClick={removeFromCart}
